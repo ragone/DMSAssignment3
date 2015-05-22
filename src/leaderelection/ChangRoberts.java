c61aa8a18c1e1aba162b91dd5e2ac664c4cdad09
jaimesBooth <jaimesbooth@192.168.1.12>
2015-05-22 14:12:15 +1200

    Moved ChangRoberts logic from ClientThread to ChangRoberts. Removed unused Bully class

--- src/leaderelection/ChangRoberts.java
+++ src/leaderelection/ChangRoberts.java
@@ -1,6 +1,11 @@
 package leaderelection;
 
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Client;
 import model.Message;
+import view.ClientThread;
 
 /**
  * Chang-Roberts leader election algorithm for a distributed system where the
@@ -10,6 +15,8 @@ import model.Message;
  *
  * @author jaimesbooth 20150516
  * @modified jaimes 20150517 Able to access sender's UUID
+ * @modified jaimes 20150522 Moved ChangRoberts logic back here
+ * @TODO Handle private messages
  */
 public class ChangRoberts
 {
@@ -18,97 +25,134 @@ public class ChangRoberts
     String ownID; // The client's unique identifier (UUID).
     String leaderID; // The current leader's unique identifier (UUID).
 
+    Client client; // a dummy client object representing the client receiving these messages
+
     /**
      * Checks the received message for leader election type messages and handles
      * the message accordingly.
+     * 
+     *  Tries to get the latest message sent to this Client (aka model)
+     * Adds message content to the GUI text field if there is a BROADCAST message.
+     * Passes on leader election messages.
      *
-     * @param changRobertsMessage An election or leader Message which enables
-     * leader election
+     * @param message An message which may contain leader election details
      */
-    public void changRobertsReceiveMessage(Message changRobertsMessage)
+    public void changRobertsReceiveMessage(Message message)
     {
-
-        if (changRobertsMessage.getType() == Message.ELECTION)
+        try
         {
-            // Election in progress so vote
-
-            // Identify UUID of client sending election message
-            String messageID = changRobertsMessage.getSenderID();
-
-            // Check if messageID > ownID
-            // String compareTo returns positive integer if this String object 
-            // lexicographically follows the argument string 
-            if (messageID.compareTo(ownID) > 0)
+            if (message.getType() == Message.ELECTION)
             {
-                // Vote for other client
-                participant = true;
-                // @TODO Send election message with messageID to next process
-            }
-            // Check if messageID = ownID
-            // String compareTo returns zero if the strings are equal 
-            else if (messageID.compareTo(ownID) == 0)
-            {
-                // This client elected leader
-                // @TODO Send leader message with ownID to next client in ring
+                // Election in progress so vote
 
-            }
-            // Check if messageID < ownID
-            // String compareTo returns negative int if this String object 
-            // lexicographically precedes the argument string 
-            else if (messageID.compareTo(ownID) < 0)
-            {
-                // This client is better
-                if (!participant)
+                // Identify UUID of client sending election message
+                String senderID = message.getSenderID();
+
+                // Check if messageID > ownID
+                // String compareTo returns positive integer if this String object 
+                // lexicographically follows the argument string 
+                if (senderID.compareTo(client.getUniqueID()) > 0)
                 {
-                    changRobertsStartElection();
+                    // Vote for sending client                  
+                    client.setParticipant(true); // This Client has now participated in the election
+
+                    // Get neighbours UUID i.e. receiver's ID
+                    String receiverID = client.getNeighbour().getUniqueID();
+
+                    // Add neighbour as receiver for this message
+                    message.setReceiverID(receiverID);
+
+                    //Send election message (containing messageID ie. the sender's ID) to the next Client
+                    client.postMessage(message);
+
+                    // @TODO handle if the Client's neighbour is null?
+                    // might not be neccessary as election would not be needed for only one Client
                 }
-                else
+                // Check if messageID = ownID
+                // String compareTo returns zero if the strings are equal 
+                else if (senderID.compareTo(client.getUniqueID()) == 0)
                 {
-                    // dont forward as already voted for better
+                    // This client elected leader
+                    // Send leader message with ownID to next client in ring
+                    String newSenderID = client.getUniqueID();
+
+                    // Get neighbours UUID i.e. receiver's ID
+                    String receiverID = client.getNeighbour().getUniqueID();
+
+                    Message leaderMessage = new Message(newSenderID, receiverID,
+                            "Leader message from" + senderID, Message.LEADER);
+                    client.postMessage(leaderMessage);
+                    // @TODO handle if the Client's neighbour is null?
+
                 }
+                // Check if messageID < ownID
+                // String compareTo returns negative int if this String object 
+                // lexicographically precedes the argument string 
+                else if (senderID.compareTo(client.getUniqueID()) < 0)
+                {
+                    // This client is a better leader
 
+                    // If this Client hasn't participated in election
+                    if (client.isParticipant() == false)
+                    {
+                        // Send election message requesting this Client
+                        // as leader.
+                        changRobertsStartElection();
+                    }
+                    // Otherwise, dont forward message as already voted 
+                    // for better leader
+                }
             }
+            else if (message.getType() == Message.LEADER)
+            {
+                // Leader has been elected
 
+                // identify UUID of Client sending leader message
+                client.setLeaderID(message.getSenderID());
+                client.setParticipant(false);
+
+                if (client.getLeaderID().equals(client.getUniqueID()))
+                {
+                    // Get neighbours UUID i.e. receiver's ID
+                    String receiverID = client.getNeighbour().getUniqueID();
+
+                    // Re-address message to next client in ring
+                    message.setReceiverID(receiverID);
+
+                    // Post leader message (with leader ID) to next client in ring
+                    client.postMessage(message);
+                    // @TODO handle if the Client's neighbour is null?
+                }
+            }
+            else
+            {
+                // Ordinary BROADCAST message received... handle as broadcast
+                //gui.getMainTextArea().append(message.getContent());
+            }
         }
-        else if (changRobertsMessage.getType() == Message.LEADER)
+        catch (RemoteException ex)
         {
-            // Leader has been elected
-
-            // identify UUID of client sending leader message
-            leaderID = changRobertsMessage.getSenderID();
-            participant = false;
-
-            if (leaderID.equals(ownID))
-            {
-                // @TODO Send leader message with leader ID to next client in ring
-            }
-        }
-        else
-        {
-            // Ordinary BROADCAST message received
-
-            // @TODO Process the message as normal: i.e Client.sendMessage()
-            /*
-            if (changRobertsMessage.getType() == Message.BROADCAST)
-            {
-                for (Map.Entry<String, LinkedList<Message>> entrySet : changRobertsMessage.entrySet())
-                {
-                    LinkedList<Message> value = entrySet.getValue();
-                    value.push(changRobertsMessage);
-                }
-            }
-            */
+            Logger.getLogger(ClientThread.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-
+    
     /**
-     * Starts the Chang-Roberts leader election algorithm from a Client.
+     * Starts the Chang-Roberts leader election process for this Client.
+     * @throws java.rmi.RemoteException
      */
-    public void changRobertsStartElection()
+    public void changRobertsStartElection() throws RemoteException
     {
-        participant = true;
+        client.setParticipant(true);
+        
+        // Get the neighbour of this Client
+        String receiverID = client.getNeighbour().getUniqueID();
 
-        // @TODO Send election message with own ID to next process in ring system
+        Message electionMessage = new Message(client.getUniqueID(), receiverID, "request vote for " 
+                + client.getUniqueID() , Message.ELECTION);
+        
+        // Post election message with own ID to next process in ring system
+        client.postMessage(electionMessage);
+        // @TODO handle if the Client's neighbour is null?
     }
 }
--- src/view/ClientThread.java
+++ src/view/ClientThread.java
@@ -14,7 +14,7 @@ import model.Message;
  * @modified jaimes 20150520 Refactored Chang-Roberts leader election algorithm to
  * post messages to neighbours.
  * @TODO Need to handle null neighbours?
- * @TODO Move Chang-Roberts receive message logic to model
+ * @modified jaimes 20150522 Moved Chang-Roberts logic to ChangRoberts class.
  */
 public class ClientThread implements Runnable {
     private final ClientGUI gui;
@@ -38,94 +38,8 @@ public class ClientThread implements Runnable {
                 Message message = model.getServer().getLastMessage(model.getUniqueID());
                 if (message != null)
                 {           
-                    if (message.getType() == Message.ELECTION)
-                    {
-                        // Election in progress so vote
-
-                        // Identify UUID of client sending election message
-                        String senderID = message.getSenderID();
-
-                        // Check if messageID > ownID
-                        // String compareTo returns positive integer if this String object 
-                        // lexicographically follows the argument string 
-                        if (senderID.compareTo(model.getUniqueID()) > 0)
-                        {
-                            // Vote for sending client                  
-                            model.setParticipant(true); // This Client has now participated in the election
-                            
-                            // Get neighbours UUID i.e. receiver's ID
-                            String receiverID = model.getNeighbour().getUniqueID();
-                            
-                            // Add neighbour as receiver for this message
-                            message.setReceiverID(receiverID);
-                            
-                            //Send election message (containing messageID ie. the sender's ID) to the next Client
-                            model.postMessage(message);
-                            
-                            // @TODO handle if the Client's neighbour is null?
-                            // might not be neccessary as election would not be needed for only one Client
-                        }
-                        // Check if messageID = ownID
-                        // String compareTo returns zero if the strings are equal 
-                        else if (senderID.compareTo(model.getUniqueID()) == 0)
-                        {
-                            // This client elected leader
-                            // Send leader message with ownID to next client in ring
-                            String newSenderID = model.getUniqueID();
-                            
-                            // Get neighbours UUID i.e. receiver's ID
-                            String receiverID = model.getNeighbour().getUniqueID();
-                            
-                            Message leaderMessage = new Message(newSenderID, receiverID,
-                                    "Leader message from" + senderID, Message.LEADER);
-                            model.postMessage(leaderMessage);
-                            // @TODO handle if the Client's neighbour is null?
-
-                        }
-                        // Check if messageID < ownID
-                        // String compareTo returns negative int if this String object 
-                        // lexicographically precedes the argument string 
-                        else if (senderID.compareTo(model.getUniqueID()) < 0)
-                        {
-                            // This client is a better leader
-                            
-                            // If this Client hasn't participated in election
-                            if (model.isParticipant() == false)
-                            {
-                                // Send election message requesting this Client
-                                // as leader.
-                                changRobertsStartElection();
-                            }
-                            // Otherwise, dont forward message as already voted 
-                            // for better leader
-                        }
-                    }
-                    else if (message.getType() == Message.LEADER)
-                    {
-                        // Leader has been elected
-
-                        // identify UUID of Client sending leader message
-                        model.setLeaderID(message.getSenderID());
-                        model.setParticipant(false);
-
-                        if (model.getLeaderID().equals(model.getUniqueID()))
-                        {
-                            // Get neighbours UUID i.e. receiver's ID
-                            String receiverID = model.getNeighbour().getUniqueID();
-                            
-                            // Re-address message to next client in ring
-                            message.setReceiverID(receiverID);
-                            
-                            // Post leader message (with leader ID) to next client in ring
-                            model.postMessage(message);
-                            // @TODO handle if the Client's neighbour is null?
-                        }
-                    }
-                    else
-                    {
-                        // Ordinary BROADCAST message received... move along
-                        gui.getMainTextArea().append(message.getContent());
-                    } 
+                    // Ordinary BROADCAST message received... move along
+                    gui.getMainTextArea().append(message.getContent());
                 }
             }
             catch (RemoteException ex)
@@ -133,24 +47,5 @@ public class ClientThread implements Runnable {
                 Logger.getLogger(ClientThread.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-    }
-    
-    /**
-     * Starts the Chang-Roberts leader election process for this Client.
-     * @throws java.rmi.RemoteException
-     */
-    public void changRobertsStartElection() throws RemoteException
-    {
-        model.setParticipant(true);
-        
-        // Get the neighbour of this Client
-        String receiverID = model.getNeighbour().getUniqueID();
-
-        Message electionMessage = new Message(model.getUniqueID(), receiverID, "request vote for " 
-                + model.getUniqueID() , Message.ELECTION);
-        
-        // Post election message with own ID to next process in ring system
-        model.postMessage(electionMessage);
-        // @TODO handle if the Client's neighbour is null?
     }
 }
